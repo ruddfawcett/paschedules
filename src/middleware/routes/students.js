8e99d5c34354519531f6ade088db2fe469ac427a
Rudd Fawcett <rudd.fawcett@gmail.com>
2016-09-05 12:53:59 -0400

    Added student page

--- src/middleware/routes/students.js
+++ src/middleware/routes/students.js
@@ -1,14 +1,37 @@
 const router = require('feathers').Router();
 const errors = require('../../utils/errors.js');
-
 module.exports = function(app) {
   const users = app.service('/api/users');
+  const sections = app.service('/api/sections');
 
   router.get('/:username', (req, res, next) => {
-    users.find({ query: { username: req.params.username, role: 'STUDENT' } }).then((users) => {
-      if (!users.data.length) { next(errors.NotFound); }
-      else {
-        res.render('schedule');
+    users.find({ query: { username: req.params.username, role: 'STUDENT' } }).then((students) => {
+      if (!students.data.length) { next(errors.NotFoundStudent); }
+         else {
+        var student = students.data[0];
+        sections.find({ query: { students: student }}).then((sections) => {
+          if (!sections.data.length) { next(errors.NotFound); }
+          else {
+            var periodZero = [];
+            var theSections = [];
+            sections.data.forEach((section) => {
+              if (section.period === 0) {
+                periodZero.push(section);
+              }
+              else {
+                theSections.push(section);
+              }
+            });
+            theSections.sort(function(a,b) {
+              return a.period > b.period;
+            });
+            res.render('student', {
+              student: student,
+              sections: theSections,
+              other: periodZero
+            });
+          }
+        });
       }
     }).catch((error) => {
       res.render('error', {
