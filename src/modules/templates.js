96c3d87ad3d6de72bba4e80e574eda03debed5e8
Rudd Fawcett <rudd.fawcett@gmail.com>
2016-09-07 16:23:04 -0400

    HTTP --> HTTPS (third time's the charm)

--- src/modules/templates.js
+++ src/modules/templates.js
@@ -1,7 +1,7 @@
 module.exports = {
   verify: {
     subject: 'Verify your account',
-    html: 'Hi %1$s,<br><br> Please verify your account with PASchedules by clicking the following URL:<br><br> <a href="http://paschedul.es/verify/%2$s">http://paschedul.es/verify/%2$s</a> <br><br>Thanks!',
-    text: 'Hi %1$s,\n\n Please verify your account with PASchedules by clicking the following URL: \n http://paschedul.es/verify/%2$s \n\nThanks!'
+    html: 'Hi %1$s,<br><br> Please verify your account with PASchedules by clicking the following URL:<br><br> <a href="https://paschedul.es/verify/%2$s">https://paschedul.es/verify/%2$s</a> <br><br>Thanks!',
+    text: 'Hi %1$s,\n\n Please verify your account with PASchedules by clicking the following URL: \n https://paschedul.es/verify/%2$s \n\nThanks!'
   }
 }
